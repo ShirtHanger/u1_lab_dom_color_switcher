21e962f730ed388a9429cd921ae3385af52a7abc
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-09-23 11:31:35 -0500

    Cannot seem to figure this out, it defaults to yellow and event listeners dont work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// Write your DOM code here!
+// Write your DOM code here
+
+const redButton = document.querySelector('.red')
+const whiteButton = document.querySelector('.white')
+const blueButton = document.querySelector('.blue')
+const yellowButton = document.querySelector('.yellow')
+
+const setColor = (colorClass) => {
+    document.body.classList.toggle(colorClass)
+}
+
+/* function setColor(colorClass) {
+    document.body.classList.toggle(colorClass)
+    // console.log(`Changed to ${colorClass}`)
+} */
+
+
+
+redButton.addEventListener('click', setColor('red'))
+whiteButton.addEventListener('click', setColor('white'))
+blueButton.addEventListener('click', setColor('blue'))
+yellowButton.addEventListener('click', setColor('yellow'))
