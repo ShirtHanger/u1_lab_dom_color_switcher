14f4cc3c3774ab1ca15fb8f622fa723f5d964652
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-09-23 12:23:11 -0500

    Instructor helped me figure out my original intended code

--- js/main_extra.js
+++ js/main_extra.js
@@ -6,13 +6,19 @@ This file was meant to test that. it failed. */
 // The problem is that ALL classes are applied as the function is
 // Immedietly invoked
 
+// UPDATE: IT WORKS NOW!
+
 const redButton = document.querySelector('.red')
 const whiteButton = document.querySelector('.white')
 const blueButton = document.querySelector('.blue')
 const yellowButton = document.querySelector('.yellow')
 
-const setColor = (colorClass) => {
+const setColor = (colorClass) => () => { // Ashley said this would fix it, no clue how...
     document.body.classList.toggle(colorClass)
+    // This makes it an anonymous function, meaning it must be specifically
+    // Called to execute, not execute immedietly
+    // Think of it as a container, will not execute
+    // Unless you call it
 }
 
 
